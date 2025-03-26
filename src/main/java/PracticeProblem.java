f8dc502c9efc5e3f1e8093e720a85d6eb1f4c107
hailey-tam-ics4u1-03-25 <hailey.tam@student.tdsb.on.ca>
2025-03-26 21:00:49 +0000

    Completed Code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,18 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
 
+	public static void insertionSort(char[] characters){
 
+		for (int i = 1; i < characters.length; i++){
+			char key = characters[i];
+			int index = i - 1;
+			while (index >= 0 && characters[index] > key){
+				characters[index + 1] = characters[index];
+				index--;
+			}
+			characters[index + 1] = key;
+		}
+	}
 }
